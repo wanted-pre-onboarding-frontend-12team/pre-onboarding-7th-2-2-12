899372489017fef5cdfd70b53de93d166a4b1dbf
OD <kg-te@naver.com>
2022-11-07 01:56:34 +0900

    🚑 수정 시 종료 날짜 사라지는 버그 수정

--- src/components/feature/Card/index.tsx
+++ src/components/feature/Card/index.tsx
@@ -2,6 +2,7 @@ import React, { Dispatch, SetStateAction, useRef, useState } from 'react';
 import Button from '@src/components/shared/Button';
 import * as S from './styled';
 import { AdProgress } from '@src/types/advertise';
+import { dateToString } from '@src/utils/DateUtils';
 
 type Props = {
 	item: AdProgress;
@@ -13,7 +14,7 @@ const Card = ({ item, data, setData }: Props) => {
 	const [editMode, setEditMode] = useState<boolean>(false);
 	const formRef = useRef<HTMLFormElement>(null);
 	const titleRef = useRef<HTMLInputElement>(null);
-	const statusRef = useRef<HTMLInputElement>(null);
+	const statusRef = useRef<HTMLSelectElement>(null);
 	const startDateRef = useRef<HTMLInputElement>(null);
 	const budgetRef = useRef<HTMLInputElement>(null);
 	const roasRef = useRef<HTMLInputElement>(null);
@@ -34,6 +35,7 @@ const Card = ({ item, data, setData }: Props) => {
 			budget: budgetRef.current?.value,
 			status: statusRef.current?.value,
 			startDate: startDateRef.current?.value,
+			endDate: item.endDate,
 			report: {
 				roas: roasRef.current?.value,
 				convValue: convValueRef.current?.value,
@@ -54,46 +56,71 @@ const Card = ({ item, data, setData }: Props) => {
 					{editMode ? (
 						<input ref={titleRef} name="title" defaultValue={item.title}></input>
 					) : (
-						<S.TitleFont>{item.title}</S.TitleFont>
+						<S.TitleFont>
+							{item.adType === 'app' ? '앱광고_' : '웹광고_'}
+							{item.title}
+						</S.TitleFont>
 					)}
 				</S.Title>
 				<S.Divider />
 				<S.BlockWrapper editMode={editMode}>
 					<S.NameFont htmlFor={item.status}>상태</S.NameFont>
-					{editMode ? <input ref={statusRef} defaultValue={item.status}></input> : <S.InfoFont>{item.status}</S.InfoFont>}
+					{editMode ? (
+						<select ref={statusRef} id="status" name="status" defaultValue={item.status}>
+							<option value="active">진행중</option>
+							<option value="ended">중단됨</option>
+						</select>
+					) : (
+						<S.InfoFont>{item.status === 'active' ? '진행중' : '중단됨'}</S.InfoFont>
+					)}
 				</S.BlockWrapper>
 				<S.Divider />
 				<S.BlockWrapper editMode={editMode}>
 					<S.NameFont htmlFor={item.startDate}>광고 생성일</S.NameFont>
 					{editMode ? (
-						<input ref={startDateRef} defaultValue={item.startDate}></input>
+						<input type="date" ref={startDateRef} defaultValue={dateToString(new Date(item.startDate))}></input>
 					) : (
-						<S.InfoFont>{item.startDate}</S.InfoFont>
+						<S.InfoFont>
+							{dateToString(new Date(item.startDate))}
+							{item.endDate ? ` (${dateToString(new Date(item.endDate))})` : ''}
+						</S.InfoFont>
 					)}
 				</S.BlockWrapper>
 				<S.Divider />
 				<S.BlockWrapper editMode={editMode}>
 					<S.NameFont htmlFor={'budget'}>일 희망 예산</S.NameFont>
-					{editMode ? <input ref={budgetRef} defaultValue={item.budget}></input> : <S.InfoFont>{item.budget}</S.InfoFont>}
+					{editMode ? (
+						<input type="number" ref={budgetRef} defaultValue={item.budget}></input>
+					) : (
+						<S.InfoFont>{item.budget}원</S.InfoFont>
+					)}
 				</S.BlockWrapper>
 				<S.Divider />
 				<S.BlockWrapper editMode={editMode}>
 					<S.NameFont htmlFor={'roas'}>광고 수익률</S.NameFont>
-					{editMode ? <input ref={roasRef} defaultValue={item.report.roas}></input> : <S.InfoFont>{item.report.roas}</S.InfoFont>}
+					{editMode ? (
+						<input type="number" ref={roasRef} defaultValue={item.report.roas}></input>
+					) : (
+						<S.InfoFont>{item.report.roas}%</S.InfoFont>
+					)}
 				</S.BlockWrapper>
 				<S.Divider />
 				<S.BlockWrapper editMode={editMode}>
 					<S.NameFont htmlFor={'convValue'}>매출</S.NameFont>
 					{editMode ? (
-						<input ref={convValueRef} defaultValue={item.report.convValue}></input>
+						<input type="number" name={'convValue'} ref={convValueRef} defaultValue={item.report.convValue}></input>
 					) : (
-						<S.InfoFont>{item.report.convValue}</S.InfoFont>
+						<S.InfoFont>{item.report.convValue}원</S.InfoFont>
 					)}
 				</S.BlockWrapper>
 				<S.Divider />
 				<S.BlockWrapper editMode={editMode}>
-					<S.NameFont htmlFor={'convValue'}>광고 비용</S.NameFont>
-					{editMode ? <input ref={costRef} defaultValue={item.report.cost}></input> : <S.InfoFont>{item.report.cost}</S.InfoFont>}
+					<S.NameFont htmlFor={'cost'}>광고 비용</S.NameFont>
+					{editMode ? (
+						<input type="number" name={'cost'} ref={costRef} defaultValue={item.report.cost}></input>
+					) : (
+						<S.InfoFont>{item.report.cost}원</S.InfoFont>
+					)}
 				</S.BlockWrapper>
 				<S.Divider />
 				<S.ButtonWrapper>
