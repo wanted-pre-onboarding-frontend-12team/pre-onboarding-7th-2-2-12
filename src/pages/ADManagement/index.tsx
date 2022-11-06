bbe00e8c8527a95e2dae5a4ffc7f205a76eb259f
OD <kg-te@naver.com>
2022-11-07 04:07:16 +0900

    ✨ 페이지 이동 시 광고 상태값 유지/ 데이터 2초 딜레이 구현

--- src/pages/ADManagement/index.tsx
+++ src/pages/ADManagement/index.tsx
@@ -1,10 +1,11 @@
 import { Layout, Button, CardList, Typography } from '@src/components';
 import { theme } from '@src/styles';
+import { getLocalStorage } from '@src/utils/StorageUtils';
 import { useState } from 'react';
 import * as S from './styled';
 
 const ADManagement = () => {
-	const [status, setStatus] = useState<string>('');
+	const [status, setStatus] = useState<string>(getLocalStorage('cardStatus') ? getLocalStorage('cardStatus') : '');
 	const handleDropBox = (e: React.ChangeEvent<HTMLSelectElement>) => {
 		setStatus(e.target.value);
 	};
@@ -14,7 +15,7 @@ const ADManagement = () => {
 			<S.Container>
 				<S.InnerContainer>
 					<S.Header>
-						<S.Select id="status" name="status" onChange={handleDropBox} defaultValue="all">
+						<S.Select id="status" name="status" onChange={handleDropBox} defaultValue="all" value={status}>
 							<option value="all">전체 광고</option>
 							<option value="active">진행중</option>
 							<option value="ended">중단됨</option>
