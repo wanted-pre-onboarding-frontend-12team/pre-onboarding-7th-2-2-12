3d3ef6d7cf35ca855ca3795621630067bd293241
jong6598 <108744804+jong6598@users.noreply.github.com>
2022-11-07 05:20:27 +0900

    :art: 통합광고현황 광고상태창 데이터 렌더링을 위한 수정

--- src/components/feature/AdStatusItem/index.tsx
+++ src/components/feature/AdStatusItem/index.tsx
@@ -1,19 +1,121 @@
 import { AdStatusWrap } from './styled';
 
-const AdStatusItem = () => {
+type Props = {
+	sum: number;
+	exSum: number;
+};
+
+export const Roas = ({ sum, exSum }: Props) => {
+	const gap = sum - exSum;
 	return (
 		<AdStatusWrap>
 			<div className="currentDiv">
-				<p>ROAS</p>
-				<span>679%</span>
+				<p>Roas</p>
+				<span>{Math.round(sum)}%</span>
 			</div>
 			<div className="compareDiv">
-				<img src="/src/assets/images/down.png" alt=""></img>
-				<img src="/src/assets/images/up.png" alt=""></img>
-				<p>18%</p>
+				{sum - exSum > 0 ? (
+					<img src="/src/assets/images/up.png" alt=""></img>
+				) : (
+					<img src="/src/assets/images/down.png" alt=""></img>
+				)}
+				<p>{Math.round(Math.abs(gap))}%</p>
 			</div>
 		</AdStatusWrap>
 	);
 };
-
-export default AdStatusItem;
+export const Cost = ({ sum, exSum }: Props) => {
+	const gap = sum - exSum;
+	return (
+		<AdStatusWrap>
+			<div className="currentDiv">
+				<p>광고비</p>
+				<span>{sum}원</span>
+			</div>
+			<div className="compareDiv">
+				{sum - exSum > 0 ? (
+					<img src="/src/assets/images/up.png" alt=""></img>
+				) : (
+					<img src="/src/assets/images/down.png" alt=""></img>
+				)}
+				<p>{Math.abs(gap)}원</p>
+			</div>
+		</AdStatusWrap>
+	);
+};
+export const Imp = ({ sum, exSum }: Props) => {
+	const gap = sum - exSum;
+	return (
+		<AdStatusWrap>
+			<div className="currentDiv">
+				<p>노출수</p>
+				<span>{sum}회</span>
+			</div>
+			<div className="compareDiv">
+				{sum - exSum > 0 ? (
+					<img src="/src/assets/images/up.png" alt=""></img>
+				) : (
+					<img src="/src/assets/images/down.png" alt=""></img>
+				)}
+				<p>{Math.abs(gap)}회</p>
+			</div>
+		</AdStatusWrap>
+	);
+};
+export const Click = ({ sum, exSum }: Props) => {
+	const gap = sum - exSum;
+	return (
+		<AdStatusWrap>
+			<div className="currentDiv">
+				<p>클릭수</p>
+				<span>{sum}회</span>
+			</div>
+			<div className="compareDiv">
+				{sum - exSum > 0 ? (
+					<img src="/src/assets/images/up.png" alt=""></img>
+				) : (
+					<img src="/src/assets/images/down.png" alt=""></img>
+				)}
+				<p>{Math.abs(gap)}회</p>
+			</div>
+		</AdStatusWrap>
+	);
+};
+export const Conv = ({ sum, exSum }: Props) => {
+	const gap = sum - exSum;
+	return (
+		<AdStatusWrap>
+			<div className="currentDiv">
+				<p>전환수</p>
+				<span>{sum}회</span>
+			</div>
+			<div className="compareDiv">
+				{sum - exSum > 0 ? (
+					<img src="/src/assets/images/up.png" alt=""></img>
+				) : (
+					<img src="/src/assets/images/down.png" alt=""></img>
+				)}
+				<p>{Math.abs(gap)}회</p>
+			</div>
+		</AdStatusWrap>
+	);
+};
+export const Revenue = ({ sum, exSum }: Props) => {
+	const gap = sum - exSum;
+	return (
+		<AdStatusWrap>
+			<div className="currentDiv">
+				<p>매출</p>
+				<span>{Math.round(sum)}원</span>
+			</div>
+			<div className="compareDiv">
+				{sum - exSum > 0 ? (
+					<img src="/src/assets/images/up.png" alt=""></img>
+				) : (
+					<img src="/src/assets/images/down.png" alt=""></img>
+				)}
+				<p>{Math.round(Math.abs(gap))}원</p>
+			</div>
+		</AdStatusWrap>
+	);
+};
