303b7eccf3a37c0803365583a67c703066f03bec
hopak-e <a328028@gmail.com>
2022-11-07 08:58:08 +0900

    💄 드롭 박스 간격 조정

---
+++ src/components/feature/Chart/styled.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const DropDownList = styled.div`
+	display: flex;
+	justify-content: space-between;
+	width: 100%;
+	padding: 2rem 1rem;
+	.ad-sort {
+		display: flex;
+		width: 260px;
+		justify-content: space-between;
+	}
+`;
